be671e5e314cd874288348a5e37560d82666428f
Ammarengineer <ammaralarashi.engineer@gmail.com>
2023-07-12 23:14:35 +0300

    my personal website is ready1

---
+++ src/tests/T1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+function Testw1() {
+  return (
+    <Row>
+      <Col sm={8} className="bg-primary">t1 </Col>
+      <Col sm={4} className="bg-secondary" >sm=4</Col>
+    </Row>
+  )
+}
+
+export default Testw1
